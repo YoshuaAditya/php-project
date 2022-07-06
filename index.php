b6e773f15eaedf357edddd77a54c324e7a6c96ca
YoshuaAditya <yoshuaadityakurnia@gmail.com>
2022-07-06 14:39:46 +0700

    Menampilkan saldo perusahaan di index/dashboard
    Hanya untuk non super-admin. Untuk super admin cuma cek echo

--- index.php
+++ index.php
@@ -53,10 +53,25 @@ checkSession();
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        <div class="card shadow mb-4">
+                            <div class="card-body" >
+                                <div class="table-responsive">
 
-                    </div>
+                                  <label for="exampleInputEmail1">Saldo: </label>
+                                  <p/>
+                                  <?php
+                                  if($_SESSION['akses']==5){
+                                    echo "SUPER ADMIN";
+                                  }
+                                  else{
+                                    echo rupiah(getJumlahSaldo($_SESSION['akses'],$connect));
+                                  }
+                                  ?><br>
+
+                                </div>
+                            </div>
+                        </div>
 
                     <!-- Content Row -->
                     <div class="row">
